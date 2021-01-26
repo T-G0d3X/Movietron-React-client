7bf0002ce3932eca42dc9bbae4e83fa2b6b678c0
Tihomir Godek <69514083+T-G0d3X@users.noreply.github.com>
2021-01-26 18:11:38 +0100

    Commits React Components, MainView, MovieCard and MovieView

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { MainView } from './components/main-view/main-view';
+
 // Import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
 
 // Main component (will eventually use all the others) 
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
-    );
+    return <MainView/>;
   }
 }
 
@@ -20,3 +18,4 @@ const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container); 
+
